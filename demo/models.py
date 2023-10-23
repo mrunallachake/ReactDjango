80cdd9b92e96048b006c5d350a799c84f50be9a0
Mrunal Lachake <mrunall@Mrunals-Air-3.lan>
2023-10-22 18:55:18 -0700

    Add book model

---
+++ demo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=36, blank=False, unique=True)
+    description = models.TextField(max_length=256, blank=True)
+    price = models.DecimalField(max_digits=3, default=0, decimal_places=2)
+    published = models.DateField(blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='covers/', blank=True)
