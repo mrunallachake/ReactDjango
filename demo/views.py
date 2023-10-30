f748c15b6de13890cd69bce984bb47c815281b20
Mrunal Lachake <mrunall@Mrunals-Air-3.lan>
2023-10-29 17:49:39 -0700

    Add book model

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
 # Create your views here.
+class Another(View):
+    books = Book.objects.all()
+    output = ""
+    for book in books:
+        output += f"{book.title}, {book.id}<br>"
+
+    def get(self,request):
+        return HttpResponse(self.output)
+    
 def first(request):
     return HttpResponse('First message from view')
